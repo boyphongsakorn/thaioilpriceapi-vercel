9ec8b82a77f193ec6f7740c28cdc60c12b081da6
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-05-21 15:17:22 +0700

    test

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const all = require('./api/all');
 
 process.env.TZ = 'Asia/bangkok';
 
+//app all Access-Control-Allow-Origin
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.json({extended: false}));
 
 app.use('/', all);
